586d2d6baccc942371f86be05b641b82a9383f6d
Jacob Schwaiger <jacob.schwaiger@ng-branch-technology.com>
2023-11-22 14:20:32 +0100

    added SymbolOpUserInterface to bf_pointer.write_ptr

--- lib/Bf/Dialect/bf_pointer/IR/BfPointerOps.cpp
+++ lib/Bf/Dialect/bf_pointer/IR/BfPointerOps.cpp
@@ -24,5 +24,25 @@ read_ptr::verifySymbolUses(SymbolTableCollection &symbolTable) {
            
   return success();
 }
+
+//===----------------------------------------------------------------------===//
+// write_ptr
+//===----------------------------------------------------------------------===//
+
+using namespace mlir::bf_pointer;
+
+mlir::LogicalResult
+write_ptr::verifySymbolUses(SymbolTableCollection &symbolTable) {
+  // Verify that the result type is same as the type of the referenced
+  // memref.global op.
+  auto bf_ptr =
+      symbolTable.lookupNearestSymbolFrom<ptr>(*this, getNameAttr());
+  if (!bf_ptr)
+    return emitOpError("'")
+           << getName() << "' does not reference a valid global bf ptr";
+
+           
+  return success();
+}
 #define GET_OP_CLASSES
 #include "Bf/Dialect/bf_pointer/IR/BfPointerOps.cpp.inc"
